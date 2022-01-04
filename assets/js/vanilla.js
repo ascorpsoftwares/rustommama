487d60884a6e9c6f18d5dc792bea8604691ab40d
Krishna <dev.ascorpgit@gmail.com>
2022-01-04 06:25:37 +0530

    added custome template for Rustom's Mama site

---
+++ assets/js/vanilla.js
@@ -0,0 +1,35 @@
+function jsCall() {
+    alert("On click call!!!");
+}
+
+function onPageLoadCall() {
+    alert("PageLoadCall");
+}
+
+var mobileMenu;
+var closeMenu;
+var hamburger;
+var navLink;
+
+window.onload = function load() {
+    hamburger = document.querySelector(".hamburger");
+    navMenu = document.querySelector(".nav-menu");
+    navLink = document.querySelectorAll(".nav-link");
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach((n) => n.addEventListener("click", closeMenu));
+};
+
+function mobileMenu() {
+    console.log("inside hamburger mobileMenu():" + hamburger.classList);
+    hamburger.classList.toggle("active");
+    // navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+function showDiv() {
+    document.getElementById('nav-menu').style.display = "block";
+}
